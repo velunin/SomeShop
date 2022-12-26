fb8d711e3784c309c6ec141b4fd983da2769209a
Vsevolod Elunin <seva.elunin@gmail.com>
2022-12-26 21:23:30 +0300

    cleanup

--- SomeShop.Common.App/Configs/KafkaConfig.cs
+++ SomeShop.Common.App/Configs/KafkaConfig.cs
@@ -4,6 +4,4 @@ public class KafkaConfig
 {
     public const string Section = "Kafka";
     public string BootstrapServers { get; set; }
-    //Hack for auto-create topic case
-    public bool IgnoreFirstUnknownTopicException { get; set; }
 }
